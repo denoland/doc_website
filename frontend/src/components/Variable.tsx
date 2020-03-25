73694560554ca2eba32a91c237dc8e6584622b9f
Luca Casonato <lucacasonato@yahoo.com>
2020-03-25 12:22:58 +0100

    Special cased constant variable namespaces

--- frontend/src/components/Variable.tsx
+++ frontend/src/components/Variable.tsx
@@ -1,16 +1,50 @@
 import React from "react";
-import { TsTypeDef, DocNodeVariable } from "../util/docs";
+import {
+  TsTypeDef,
+  DocNodeVariable,
+  TsTypeDefKind,
+  TsTypeDefTypeLiteral
+} from "../util/docs";
 import { SimpleLink } from "./SimpleLink";
 import { Page } from "./Page";
 import { JSDoc } from "./JSDoc";
-import { SimpleCard } from "./SinglePage";
+import { SimpleCard, SimpleSubCard } from "./SinglePage";
 
 export function VariableCard({ node }: { node: DocNodeVariable }) {
+  const type = node.variableDef.tsType;
+  const isNamespace =
+    node.variableDef.kind === "const" &&
+    type.kind === TsTypeDefKind.TypeLiteral &&
+    type.typeLiteral.properties.length > 0 &&
+    type.typeLiteral.methods.length === 0 &&
+    type.typeLiteral.callSignatures.length === 0;
+
+  return isNamespace ? (
+    <VariableNamespaceCard node={node} />
+  ) : (
+    <SimpleCard node={node} prefix={node.variableDef.kind} returnType={type} />
+  );
+}
+
+export function VariableNamespaceCard({ node }: { node: DocNodeVariable }) {
+  const type = node.variableDef.tsType as TsTypeDefTypeLiteral;
   return (
     <SimpleCard
       node={node}
       prefix={node.variableDef.kind}
-      returnType={node.variableDef.tsType}
+      details={
+        <div className="mt-2">
+          <p className="text-md font-medium">Properties</p>
+          {type.typeLiteral.properties.map(node => {
+            return (
+              <SimpleSubCard
+                node={{ name: node.name, snippet: "" }}
+                returnType={node.tsType}
+              />
+            );
+          })}
+        </div>
+      }
     />
   );
 }
