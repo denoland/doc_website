1fbc7365bdc6253ab98de0d92816052d99c2b571
Luca Casonato <lucacasonato@yahoo.com>
2020-03-29 12:33:07 +0200

    Remove rust component (#29)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Example output in `docs.json`
 
 `index.html` has simple webpage with output.
+
+Rust component moved to https://github.com/denoland/deno/tree/master/cli/doc
--- src/printer/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::doc;
-
-pub mod json;
-pub mod terminal;
-
-pub trait Printer {
-  fn print(&self, doc_nodes: Vec<doc::DocNode>);
-  fn print_details(&self, node: doc::DocNode);
-}
