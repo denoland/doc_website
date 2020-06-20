5cb0ec6fd8dd655ebe96245ffd5f587b9b95fbed
Luca Casonato <luca.casonato@antipy.com>
2020-06-21 00:00:31 +0200

    Match design with main site (#117)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,12 @@
 
 import React, { useMemo } from "react";
 import Link from "next/link";
-import { groupNodes, DocNodeShared, sortByAlphabet } from "../util/docs";
+import {
+  groupNodes,
+  DocNodeShared,
+  sortByAlphabet,
+  GroupedNodes,
+} from "../util/docs";
 import { useFlattend } from "../util/data";
 
 const SidebarSection = (props: {
@@ -11,10 +16,8 @@ const SidebarSection = (props: {
   nodes: DocNodeShared[];
 }) => {
   return props.nodes.length > 0 ? (
-    <div className="py-4">
-      <div className="mb-1 text-xl font-medium text-gray-900">
-        {props.title}
-      </div>
+    <div className="mt-2 mb-4">
+      <div className="mb-1 text-lg text-gray-900 font-bold">{props.title}</div>
       <div>
         {props.nodes.map((node, i) => {
           const scope = node.scope ? node.scope.join(".") + "." : "";
@@ -32,41 +35,10 @@ const SidebarSection = (props: {
   ) : null;
 };
 
-export const Sidebar = (props: {
-  forceReload: () => void;
-  entrypoint: string;
-  timestamp: string;
-}) => {
-  const flattend = useFlattend();
-  const nodes = useMemo(() => sortByAlphabet(flattend), [flattend]);
-  const groups = useMemo(() => groupNodes(nodes), [nodes]);
-
+export const Sidebar = ({ groups }: { groups: GroupedNodes }) => {
   return (
-    <>
-      {flattend ? (
-        <header className="px-4 pt-3 sm:px-6 sm:pt-4">
-          <div className="text-xl font-bold text-gray-900 lg:hidden">
-            Table of Contents
-          </div>
-          {props.timestamp ? (
-            <div className="mt-1 text-sm text-gray-600">
-              Last refreshed {new Date(props.timestamp).toLocaleString()}.{" "}
-              <a
-                className="text-sm cursor-pointer link"
-                onClick={props.forceReload}
-              >
-                Refresh now
-              </a>
-            </div>
-          ) : null}
-          <p className="mt-2 text-sm">
-            <Link href="/about">
-              <a className="link">About doc.deno.land</a>
-            </Link>
-          </p>
-        </header>
-      ) : null}
-      <nav className="px-4 py-2 sm:px-6">
+    <div className="pt-2 pb-8 h-0 flex-1 flex flex-col overflow-y-auto">
+      <nav className="flex-1 px-4">
         <>
           <SidebarSection
             title="Functions"
@@ -97,6 +69,6 @@ export const Sidebar = (props: {
           nodes={groups.namespaces}
         />
       </nav>
-    </>
+    </div>
   );
 };
