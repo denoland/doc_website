6a4c29638db1d1d8231ccaf782e554192f6f4d8a
Luca Casonato <lucacasonato@yahoo.com>
2020-08-07 13:58:38 +0200

    Add special builtin page

---
+++ pages/builtin/[version].tsx
@@ -0,0 +1,23 @@
+// Copyright 2020 the Deno authors. All rights reserved. MIT license.
+
+import { useRouter } from "next/router";
+import { Documentation } from "../../components/Documentation";
+
+const Page = () => {
+  const { query } = useRouter();
+  const version = query.version ?? "stable";
+  return (
+    <Documentation
+      entrypoint={
+        version == "stable"
+          ? "https://github.com/denoland/deno/releases/latest/download/lib.deno.d.ts"
+          : version === "unstable"
+          ? "https://raw.githubusercontent.com/denoland/deno/master/cli/dts/lib.deno.unstable.d.ts"
+          : `https://github.com/denoland/deno/releases/download/${version}/lib.deno.d.ts`
+      }
+      name={`builtin@${version}`}
+    />
+  );
+};
+
+export default Page;
