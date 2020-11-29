251f6483b59063e9ef4075f89a7a0ee5cdb99338
Kid <44045911+kidonng@users.noreply.github.com>
2020-11-29 13:46:09 +0800

    Allow GitHub/GitLab's blob page as URL

--- pages/https/[...url].tsx
+++ pages/https/[...url].tsx
@@ -8,12 +8,18 @@ const Page: NextPage<{ entrypoint: string; name: string }> = (props) => (
 );
 
 Page.getInitialProps = async (ctx) => {
-  const url =
+  let url =
     typeof ctx.query.url === "string"
       ? ctx.query.url
       : ctx.query.url === undefined
       ? ""
       : ctx.query.url.join("/");
+  
+  const regex = /(github\.com\/.+\/.+\/|gitlab\.com\/.+\/.+\/-\/)blob/
+  if (url.match(regex)) {
+    url = url.replace(regex, "$1raw")
+  }
+  
   return { entrypoint: "https://" + url, name: url };
 };
 
