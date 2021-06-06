0fc1681d79e9c560cbfa7c6b7b0273a17d591482
Zeb Piasecki <zeb@zebulon.dev>
2021-06-06 09:13:52 -0400

    fix: evict old documentation from local storage cache when low on space

--- util/data.ts
+++ util/data.ts
@@ -36,13 +36,11 @@ export async function getData(
   // TODO: Use a smarter way to ensure that the entrypoint is tagged with a version, currently we
   // will get occasional cache misses for version-tagged entrypoints.
   const canBeCached = HAS_VERSION_REGEX.test(entrypoint);
-
+  const cacheKey = `cached-entrypoint-${entrypoint}`;
   // Looks for the data cached in the local storage if we know that the entrypoint is tagged with a
   // version and we aren't forcing a reload.
   const cachedData =
-    canBeCached && !forceReload
-      ? window.localStorage.getItem(entrypoint)
-      : null;
+    canBeCached && !forceReload ? window.localStorage.getItem(cacheKey) : null;
 
   if (cachedData) {
     return JSON.parse(cachedData);
@@ -59,12 +57,47 @@ export async function getData(
       nodes: resp.nodes,
     };
 
+    const dataStringified = JSON.stringify(data);
+
     if (canBeCached) {
       try {
-        window.localStorage.setItem(entrypoint, JSON.stringify(data));
+        window.localStorage.setItem(cacheKey, dataStringified);
       } catch (error) {
-        // The local storage is either full or the user has disabled it's usage, so we'll just
-        // ignore this error and not cache our result.
+        // The local storage is either full or the user has disabled it's usage. We'll try to evict
+        // some of the oldest items in the cache to make space.
+
+        // Reads all of the cached items in the local storage and pairs them with the data and the
+        // size of the entry.
+        const cacheTuples = [...new Array(window.localStorage.length)]
+          .map((_, i) => window.localStorage.key(i))
+          .filter(
+            (key): key is string =>
+              key?.startsWith("cached-entrypoint-") ?? false
+          )
+          .map((key): [string, DocsData, number] => {
+            const data = window.localStorage.getItem(key)!;
+            return [key, JSON.parse(data) as DocsData, data.length];
+          })
+          .sort(
+            ([_aKey, a], [_bKey, b]) =>
+              Date.parse(a.timestamp) - Date.parse(b.timestamp)
+          );
+
+        let deletedData = 0;
+
+        // Removes items from the local storage until we have enough to store the new data.
+        for (const [key, data, sizeInStorage] of cacheTuples) {
+          if (deletedData >= dataStringified.length) {
+            break;
+          }
+
+          console.log(data);
+          window.localStorage.removeItem(key);
+          deletedData += sizeInStorage;
+        }
+
+        // Tries to cache the key again, after clearing enough space for it.
+        window.localStorage.setItem(cacheKey, dataStringified);
       }
     }
 
